f982d02e8792df83e461877134454bdfc13f85bf
samsun076 <davemarcinowski@gmail.com>
2023-11-19 20:18:31 -0500

    added letter counter with dict

--- frankenstein02.py
+++ frankenstein02.py
@@ -4,7 +4,10 @@ def main():
     book = "books/frankenstein.txt"
     frankenstein = get_book_text(book)
     num_words = get_word_count(frankenstein)
+    letter_count = get_letter_count(frankenstein)
     print(f"{num_words} words found in {book}")
+    print("###")
+    print(letter_count)
 
 def get_book_text(book_name):
     with open(book_name) as f:
@@ -12,6 +15,15 @@ def get_book_text(book_name):
 
 def get_word_count(book):
     return len(book.split())
+
+def get_letter_count(book):
+    counter = {}
+    for letter in book.lower():
+        if letter in counter:
+            counter[letter] += 1
+        else:
+            counter[letter] = 1
+    return counter
 
 main()
 
---
+++ scrappy.py
@@ -0,0 +1,14 @@
+my_string = "aaAAa vVVvv bBBbb sSSsss lLll"
+book = "books/frankenstein.txt"
+
+counter = {}
+for letter in my_string.lower():
+    if letter in counter:
+        counter[letter] += 1
+    else:
+        counter[letter] = 1
+    
+for k,v in counter.items():
+    print(f"{k} -- {v}")
+
+
