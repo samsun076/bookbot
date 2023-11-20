88ed45dd027540c85f7b397f5c4fc909486ef401
samsun076 <davemarcinowski@gmail.com>
2023-11-20 15:39:37 -0500

    final verson of bookbot

--- bookbot03.py
+++ bookbot03.py
@@ -6,15 +6,30 @@ def main():
     frankenstein = get_book_text(book)
     num_words = get_word_count(frankenstein)
     letter_count = get_letter_count(frankenstein)
-    
+    sorted_letter_count = letter_count_to_sorted_list(letter_count)
+
     print(f"--- Begin report of {book} --- ")
     print(f"{num_words} were found in the document \n")
-    book_letter = list(letter_count.keys())
-    book_letter.sort()
-    for letter in book_letter:
-        if letter.isalpha():
-            print(f"The '{letter}' character was found {letter_count[letter]} times")
+    
+    
+    for x in sorted_letter_count:
+        if x["char"].isalpha():
+            print(f"The {x['char']} character was found {x['num']}")
+       
+    
     print(f"--- End report ---")
+
+
+def sort_on(d):
+    return d["num"]
+
+def letter_count_to_sorted_list(letter_count_dict):
+    sorted_list = []
+    for ch in letter_count_dict:
+        sorted_list.append({"char":ch, "num":letter_count_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
 
 def get_book_text(book_name):
     with open(book_name) as f:
