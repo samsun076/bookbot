7057d1522445cd9ae8b99eb8773f1a1b197020f8
samsun076 <davemarcinowski@gmail.com>
2023-11-21 06:57:34 -0500

    median exercise in scrap file

--- scrappy.py
+++ scrappy.py
@@ -1,18 +1,11 @@
-
-print("SCRAPPY")
-my_string = "aaAAa vVVvv bBBbb sSSsss lLll"
-book = "books/frankenstein.txt"
-
-counter = {}
-for letter in my_string.lower():
-    if letter in counter:
-        counter[letter] += 1
+def median(data):
+    N = len(sorted(data))
+    if N%2==1:
+        return data[N//2]
     else:
-        counter[letter] = 1
-    
-for k,v in counter.items():
-    print(f"{k} -- {v}")
+        return "even"
 
 
-print("#### --END-- ###")
+my_list = [15,1,2,3,4,14,5,6,7,8,9,10,11,12,13]
 
+print(median(my_list))
